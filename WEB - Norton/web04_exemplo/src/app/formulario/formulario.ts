2b5c68a64dd9432922a733ed06886cee22824a10
Aluno <aluno@IPIRANGA.FATEC>
2026-09-15 11:58:39 -0300

    2S

---
+++ WEB - Norton/web04_exemplo/src/app/formulario/formulario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-formulario',
+  styleUrl: './formulario.css',
+  templateUrl: './formulario.html',
+})
+export class Formulario {}
---
+++ WEB - Norton/web04_exemplo/src/app/principal/principal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-principal',
+  styleUrl: './principal.css',
+  templateUrl: './principal.html',
+})
+export class Principal {}
